7335e7dcb8e6325ddf2bdac2b054668411e53803
Nicolas <2000nsalgado@gmail.com>
2024-07-16 10:09:34 -0300

    graphic-update

--- main-dashboard/dashboard.js
+++ main-dashboard/dashboard.js
@@ -3,10 +3,10 @@ const ctx = document.getElementById('grafico1');
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Sep', 'Oct', 'Nov', 'Dez'],
       datasets: [{
         label: 'Custos',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [1200, 528, 3852, 5324, 2344, 3455, 4534, 4213, 4564, 1324, 5657],
         backgroundColor: 'rgba(128, 128, 128, 0.5)', // Cor cinza
         borderColor: 'rgba(128, 128, 128, 1)', // Cor cinza
         borderWidth: 1
