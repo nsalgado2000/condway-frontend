1b0e80a09be1d5a2a891db56fc47bc2f2f891548
Nicolas <2000nsalgado@gmail.com>
2024-07-16 08:19:11 -0300

    bar-change-to-gray

--- main-dashboard/dashboard.js
+++ main-dashboard/dashboard.js
@@ -5,8 +5,10 @@ const ctx = document.getElementById('grafico1');
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-        label: '# of Votes',
+        label: 'Custos',
         data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: 'rgba(128, 128, 128, 0.5)', // Cor cinza
+        borderColor: 'rgba(128, 128, 128, 1)', // Cor cinza
         borderWidth: 1
       }]
     },
@@ -18,3 +20,4 @@ const ctx = document.getElementById('grafico1');
       }
     }
   });
+  
