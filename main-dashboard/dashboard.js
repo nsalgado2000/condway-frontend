b08e7f1e68afa0858ac14ca227787af3c105e93a
Nicolas <2000nsalgado@gmail.com>
2024-07-17 00:59:02 -0300

    fim-da-jornada-de-trabalho

--- main-dashboard/dashboard.js
+++ main-dashboard/dashboard.js
@@ -3,11 +3,11 @@ const ctx = document.getElementById('grafico1');
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Sep', 'Oct', 'Nov', 'Dez'],
+      labels: ['Água', 'Luz', 'Condomínio', 'Gás', 'Manutenção'],
       datasets: [{
-        label: 'Custos',
-        data: [1200, 528, 3852, 5324, 2344, 3455, 4534, 4213, 4564, 1324, 5657],
-        backgroundColor: 'rgba(128, 128, 128, 0.5)', // Cor cinza
+        label: ': Valor em R$',
+        data: [1200, 528, 3852, 5324, 2344],
+        backgroundColor: 'rgba(40, 85, 222, 0.5)', // Cor cinza
         borderColor: 'rgba(128, 128, 128, 1)', // Cor cinza
         borderWidth: 1
       }]
