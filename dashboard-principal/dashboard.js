f5c15e5a09d3eb48e1faae6b8e8150b9c68f86c0
Nicolas <2000nsalgado@gmail.com>
2024-07-18 19:18:00 -0300

    💄 feat: Atualização no modelo mobile do Dashboard Principal

--- dashboard-principal/dashboard.js
+++ dashboard-principal/dashboard.js
@@ -20,4 +20,31 @@ const ctx = document.getElementById('grafico1');
       }
     }
   });
+
+  function adjustLayoutForMobile() {
+    const containerWidth = window.innerWidth;
+    const graficobarra =  document.getElementById('graficobarra')
+    const mensagens = document.getElementById('mensagens')
+    const titulo = document.getElementsByTagName('span')
+    const resumo = document.getElementById('resumo')
+    if (containerWidth <= 600) {
+    
+        mensagens.style.width = '90%';
+        mensagens.style.marginLeft = '25px';
+        mensagens.style.height = '300px';
+        graficobarra.style.width = '90%';
+        graficobarra.style.padding = '2px'
+        titulo[5].style.fontSize = 'px'
+      } else{
+        mensagens.style.width = '635px';
+        mensagens.style.marginLeft = '50px';
+        mensagens.style.height = '445px';
+        graficobarra.style.width = '800px';
+        graficobarra.style.padding = '20px'
+      }
+      }
+  
+
+  window.addEventListener('load', adjustLayoutForMobile);
+  window.addEventListener('resize', adjustLayoutForMobile);
   
